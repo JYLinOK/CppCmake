32201b5b8ecca9fc83dda8df427786bca18d3217
JYLinOK <ydookm@163.com>
2023-10-29 11:50:45 +0800

    Start

---
+++ CMakeFiles/cppcmake1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppcmake1.dir/src/cpp1_make_a_cpp_file.o"
+  "CMakeFiles/cppcmake1.dir/src/cpp1_make_a_cpp_file.o.d"
+  "cppcmake1"
+  "cppcmake1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppcmake1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
