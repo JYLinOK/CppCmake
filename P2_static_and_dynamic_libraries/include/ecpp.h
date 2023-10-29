d1a11df4d16933d9314c20f7086fa2d9657ec06c
JYLinOK <ydookm@163.com>
2023-10-29 22:49:44 +0800

    add ecpp.h

---
+++ P2_static_and_dynamic_libraries/include/ecpp.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#define V_NAME(value) (#value)
+
+using namespace std;
+
+void print()
+{
+    cout << endl;
+}
+
+template <typename T, typename... Targs>
+void print(T value, Targs... args)
+{
+    cout << value << ' ';
+
+    if ((sizeof...(args)) > 0)
+    {
+        print(args...); // When there are multiple parameters, continue printing with print()
+    }
+    else
+    {
+        print(); // When there is no parameter, directly print endl;
+    }
+}
+
+/// <summary>
+/// print the type of a variable
+/// </summary>
+void print_type()
+{
+    cout << endl;
+}
+
+template <typename T, typename... Targs>
+void print_type(T value, Targs... args)
+{
+    cout << V_NAME << " type = " << typedef.id(value).name();
+
+    if ((sizeof...(args)) > 0)
+    {
+        print_type(args...); // // When there are multiple parameters, continue printing with print_type()
+    }
+    else
+    {
+        print_type(); // When there is no parameter, directly print endl;
+    }
+}
